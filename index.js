684a0ca5498faf99fac6814b0efc85c4ef646959
Biraj <biraj.pub@gmail.com>
2023-02-24 17:11:15 +0530

    started using mongo in project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 import setup from "./setup/setup.js";
 import log from "./util/log.js";
 import toEnv from "./util/toEnv.js";
@@ -12,7 +13,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/admin/config", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "index.html"));
+  res.sendFile(path.join(process.cwd(), "config.html"));
 });
 
 app.post("/admin/config", async (req, res) => {
@@ -53,9 +54,13 @@ app.get("/api/test", (req, res) => {
 try {
   await setup();
   dotenv.config({ override: true });
+
+  await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+  log("connected with mongo");
+
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => log(`server running on port ${PORT}...`));
 } catch (err) {
-  log("error(s) occured during setup");
+  log("error occured while starting the server");
   log(err.message.trim());
 }
