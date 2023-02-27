ada8394537d53cd19f4da1a05fb3b989ff4d3db8
Biraj <biraj.pub@gmail.com>
2023-02-27 17:19:41 +0530

    log msgs improved

--- setup/mongo.js
+++ setup/mongo.js
@@ -11,17 +11,18 @@ export default async function mongoSetup() {
   const client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
   try {
     await client.connect();
-    log("connected to mongodb server for setup");
+    log("mongo-setup > connected to mongodb server");
+
     const db = client.db(DB_NAME);
     const existingUser = (await db.command({ usersInfo: { user: DB_USER, db: DB_NAME } })).users[0];
     if (existingUser) {
-      log(`user ${DB_USER} already exists in database ${DB_NAME}`);
+      log(`mongo-setup > user ${DB_USER} already exists in database ${DB_NAME}`);
       return;
     }
 
     // create mongodb user
     await db.addUser(DB_USER, DB_PASS, { roles: ["dbOwner"] });
-    log(`User ${DB_USER} has been created in ${DB_NAME}`);
+    log(`mongo-setup > user ${DB_USER} has been created in db ${DB_NAME}`);
 
     // enable mongodb authorization
     const mongoConfYaml = await fs.readFile("/etc/mongod.conf", "utf8");
@@ -32,20 +33,20 @@ export default async function mongoSetup() {
     // restart mongodb server
     exec("systemctl restart mongod.service", (err, stdout, stderr) => {
       if (err) {
-        log(err);
+        log(`mongo-setup > restart > ${err.message}`);
         return;
       }
 
       if (stdout) {
-        log(`mongo-setup > stdout: ${stdout}`);
+        log(`mongo-setup > restart > stdout: ${stdout}`);
       }
 
       if (stderr) {
-        log(`mongo-setup > stderr: ${stderr}`);
+        log(`mongo-setup > restart > stderr: ${stderr}`);
       }
     });
   } catch (err) {
-    log(err.message);
+    log(`mongo-setup > ${err.message}`);
   } finally {
     client.close();
   }
