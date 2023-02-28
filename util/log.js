be3a7dcfa1e26cc79c6c2f146f53f854c8f515dd
Biraj <biraj.pub@gmail.com>
2023-02-28 17:51:38 +0530

    set file permission 644 of log file now

--- util/log.js
+++ util/log.js
@@ -4,5 +4,7 @@ import path from "node:path";
 export default async function log() {
   const now = new Date();
   const str = `${now.toDateString()} ${now.toLocaleTimeString()}: ${Array.from(arguments).join(" ")}\n`;
-  fs.appendFileSync(path.join(process.cwd(), "server.log"), str);
+  fs.appendFileSync(path.join(process.cwd(), "server.log"), str, {
+    mode: 0b110100100,
+  });
 }
