a292849ca63db12e82163a75fce71619263fb841
Biraj <biraj.pub@gmail.com>
2023-03-09 10:45:00 +0530

    no error if directory exists

--- machine-id/machine-id.sh
+++ machine-id/machine-id.sh
@@ -1,5 +1,5 @@
 sno=$(/usr/sbin/dmidecode -t 2 | /usr/bin/grep -oP "Serial Number: \K[a-zA-Z0-9]+")
-mkdir /pickcel
+mkdir -p /pickcel
 echo "$sno" >> /pickcel/machine-id
 chmod 600 /pickcel/machine-id
 systemctl disable machine-id.service
